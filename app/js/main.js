d3be22f84ab3ed0f828fbd3d17f9db63395553c1
alexta43 <alexta430@gmail.com>
2021-03-03 00:51:37 +0300

    added blog-one

--- app/js/main.js
+++ app/js/main.js
@@ -43,6 +43,16 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000
   });
+  $('.blog-page__slider').slick({
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="9pt" height="16pt" viewBox="0 0 9 16" version="1.1"><g><path d="M 1.113281 7.46875 L 5.894531 3.21875 C 6.226562 2.925781 6.761719 2.925781 7.085938 3.21875 L 7.882812 3.925781 C 8.210938 4.21875 8.210938 4.695312 7.882812 4.984375 L 4.496094 8 L 7.886719 11.011719 C 8.214844 11.304688 8.214844 11.78125 7.886719 12.070312 L 7.089844 12.78125 C 6.761719 13.074219 6.226562 13.074219 5.898438 12.78125 L 1.117188 8.53125 C 0.785156 8.238281 0.785156 7.761719 1.113281 7.46875 Z M 1.113281 7.46875 "/></g></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="9pt" height="16pt" viewBox="0 0 9 16" version="1.1"><g><path d="M 7.886719 8.53125 L 3.105469 12.78125 C 2.773438 13.074219 2.238281 13.074219 1.914062 12.78125 L 1.117188 12.074219 C 0.789062 11.78125 0.789062 11.304688 1.117188 11.015625 L 4.507812 8.003906 L 1.117188 4.992188 C 0.789062 4.695312 0.789062 4.222656 1.117188 3.929688 L 1.910156 3.21875 C 2.238281 2.925781 2.773438 2.925781 3.101562 3.21875 L 7.882812 7.46875 C 8.214844 7.761719 8.214844 8.238281 7.886719 8.53125 Z M 7.886719 8.53125 "/></g></svg></button>'
+  });
+
+  $('.slick-arrow').on('click', function() {
+    $('.slick-arrow').removeClass('slick-arrow--active');
+    $(this).addClass('slick-arrow--active')
+  });
 
   $('.select-style, .product-one__item-num').styler();
 
